a452d6f30f090e3e09582fb853ab8a93d8dd9ae5
Juan Saldana <saldanaj97@gmail.com>
2023-02-24 13:41:55 -0600

    Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import Head from "next/head";
 
 export default function Home() {
   const [surveyCompletedState, setSurveyCompletedState] = useState(false);
+  const [userSelection, setUserSelection] = useState("0");
 
   const surveyCompleted = () => {
     setSurveyCompletedState(true);
+  };
+
+  const updateUserSelection = (selection) => {
+    setUserSelection(selection);
   };
 
   const SurveyDisplay = () => {
@@ -19,24 +24,26 @@ export default function Home() {
           <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering! </p>
         </div>
         <div className='rating-btn-container'>
-          <div className='rating-btn'>
-            <p className='btn-text'>1</p>
-          </div>
-          <div className='rating-btn'>
-            <p className='btn-text'>2</p>
-          </div>
-          <div className='rating-btn'>
-            <p className='btn-text'>3</p>
-          </div>
-          <div className='rating-btn'>
-            <p className='btn-text'>4</p>
-          </div>
-          <div className='rating-btn'>
-            <p className='btn-text'>5</p>
-          </div>
+          <button className={userSelection === 1 ? "rating-btn-selected" : "rating-btn"} onClick={() => updateUserSelection(1)}>
+            <p className='rating-btn-text'>1</p>
+          </button>
+          <button className={userSelection === 2 ? "rating-btn-selected" : "rating-btn"} onClick={() => updateUserSelection(2)}>
+            <p className='rating-btn-text'>2</p>
+          </button>
+          <button className={userSelection === 3 ? "rating-btn-selected" : "rating-btn"} onClick={() => updateUserSelection(3)}>
+            <p className='rating-btn-text'>3</p>
+          </button>
+          <button className={userSelection === 4 ? "rating-btn-selected" : "rating-btn"} onClick={() => updateUserSelection(4)}>
+            <p className='rating-btn-text'>4</p>
+          </button>
+          <button className={userSelection === 5 ? "rating-btn-selected" : "rating-btn"} onClick={() => updateUserSelection(5)}>
+            <p className='rating-btn-text'>5</p>
+          </button>
         </div>
         <div className='submit-btn-container' onClick={surveyCompleted}>
-          <button className='submit-btn'>S U B M I T</button>
+          <button className='submit-btn'>
+            <p className='submit-btn-text'>S U B M I T</p>
+          </button>
         </div>
       </div>
     );
@@ -49,7 +56,7 @@ export default function Home() {
           <img src='../images/illustration-thank-you.svg' />
         </div>
         <div className='user-survey-selection-container'>
-          <div className='survey-selection-text'>You selected 4 out of 5</div>
+          <div className='survey-selection-text'>You selected {userSelection} out of 5</div>
         </div>
         <div className='thank-you-heading'>
           <h1>Thank you!</h1>
